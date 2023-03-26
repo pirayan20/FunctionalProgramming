144d529e6b70d1a350b6a127df917a1a8c66ac65
pirayan20 <90704316+pirayan20@users.noreply.github.com>
2023-03-26 17:34:57 +0700

    .

--- Homework/Question3.scala
+++ Homework/Question3.scala
@@ -1,7 +1,21 @@
 package Homework
 
 object Question3 {
+  def member(x: Any, l: List[Any]): Boolean = {
+    if (l.isEmpty) false
+    if (l.head == x) true
+    else member(x, l.tail)
+  }
+
   def subList(l1:List[Any], l2: List[Any]) : Boolean = {
-    if (l1.isEmpty) true
+    if (l1.isEmpty) return true
+    else if (member(l1.head,l2)) subList(l1.tail,l2)
+    else false
+  }
+
+  def main(args: Array[String]) : Unit = {
+    var l1 = List(7)
+    var l2 = List(1,2,3,4,5)
+    println(subList(l1,l2))
   }
 }
---
+++ Homework/Question5.scala
@@ -0,0 +1,25 @@
+package Homework
+
+object Question5 {
+
+  def myReverse(l: List[Any]): List[Any] = {
+    if (l.isEmpty) return List()
+    myReverse(l.tail) ++ List(l.head)
+  }
+
+  def palindrome(list: List[Any]) : Boolean = {
+    var reverse = myReverse(list)
+    palindrome(list,reverse)
+  }
+
+  def palindrome(l1: List[Any],l2: List[Any]): Boolean = {
+    if (l1.isEmpty || l2.isEmpty) true
+    if (l1.head == l2.head) palindrome(l1.tail,l2.tail)
+    else false
+  }
+
+  def main(args: Array[String]) : Unit = {
+    val l = List(1,2,3,5)
+    println(palindrome(l))
+  }
+}
