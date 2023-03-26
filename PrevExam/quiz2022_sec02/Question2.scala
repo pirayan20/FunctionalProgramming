ce0b92fa410695ded6d97590b5f151233f3a7f38
π <pirayan20@gmail.com>
2023-03-27 02:45:03 +0700

    Update Question2.scala

--- PrevExam/quiz2022_sec02/Question2.scala
+++ PrevExam/quiz2022_sec02/Question2.scala
@@ -14,14 +14,27 @@ object Question2 {
       if (member(l1.head,l2)) helper(l1.tail,l2,acc+1)
       else helper(l1.tail,l2,acc)
     }
-    println(helper(l1,l2,0))
-    if (helper(l1,l2,0) >= (l1.length/2)) true
+    var ans = helper(l1,l2,0)
+    if (l1.length % 2 == 1) ans-=1
+    if (ans >= (l1.length/2)) true
     else false
   }
 
   def main(args: Array[String]) : Unit = {
-    val l1 = List(1,4,3,2,7,8)
-    val l2 = List(2,9,7,1,6,10)
-    println(moreThanHalf(l1,l2))
+    val l00 = List();
+    val l01 = List();
+    val l1 = List(1, 4, 3, 2, 7, 8)
+    val l2 = List(2, 9, 7, 1, 6, 10)
+    val l3 = List(12, 11, 6, 5, 3, 2)
+    val l4 = List("1", "2", "3", "4", "5")
+    val l5 = List("9", "7", "5", "3", "1")
+    val l6 = List("2", "4", "6", "8", "10")
+
+    println(moreThanHalf(l00, l01))
+    println(moreThanHalf(l1, l01))
+    println(moreThanHalf(l1, l2))
+    println(moreThanHalf(l1, l3))
+    println(moreThanHalf(l4, l5))
+    println(moreThanHalf(l4, l6))
   }
 }
